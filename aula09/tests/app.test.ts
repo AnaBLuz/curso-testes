d4db757434ad697793bff9092acb6a4f4dfeb22f
Bia Luz <anabluz08@gmail.com>
2023-10-06 01:04:49 -0300

    aula09

--- aula09/tests/app.test.ts
+++ aula09/tests/app.test.ts
@@ -1,5 +1,5 @@
 import supertest from "supertest";
-
+import { UserInput } from "../src/repository";
 import app from "./../src/app";
 import prisma from "../src/database";
 
@@ -11,26 +11,68 @@ beforeEach(async () => {
 
 describe("POST /users tests", () => {
   it("should create a user", async () => {
-    // TODO
+    const user: UserInput = {
+      email: "email@exemplo.com",
+      password:"12345"
+    }
+    const { status } = await api.post("/users").send(user);
+    expect(status).toBe(201) 
   });
 
   it("should receive 409 when trying to create two users with same e-mail", async () => {
-    // TODO
+    const user1: UserInput = {
+      email: "email@exemplo.com",
+      password:"12345"
+    }
+
+    await api.post("/users").send(user1);
+    
+    const user2: UserInput = {
+      email: "email@exemplo.com",
+      password:"67687"
+    }
+    const { status } = await api.post("/users").send(user2);
+    expect(status).toBe(409)
   });
 
 });
 
 describe("GET /users tests", () => {
   it("should return a single user", async () => {
-    // TODO
+    const user: UserInput = {
+      email: "email@exemplo.com",
+      password:"12345"
+    }
+    const userCreated = await prisma.user.create({
+      data: user
+    });
+    const {body, status} = await api.get(`/users/${userCreated.id}`);
+    expect(status).toBe(200)
+    expect(body).toEqual(userCreated)
   });
 
   it("should return 404 when can't find a user by id", async () => {
-    // TODO
+    const { status } = await api.get("/users/2");
+    expect(status).toBe(404);
   });
 
   it("should return all users", async () => {
-    // TODO
+    const user: UserInput = {
+      email: "email@exemplo.com",
+      password:"12345"
+    }
+    const userCreated = await prisma.user.create({
+      data: user
+    });
+    const { body, status } = await api.get("/users");
+    expect(status).toBe(200);
+    expect(body).toHaveLength(1);
+    expect(body).toEqual(expect.arrayContaining([
+      expect.objectContaining({
+        email: expect.any(String)
+      })
+    ]))
+
   });
 
 })
